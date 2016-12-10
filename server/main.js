440b6a3555289c306d8e28360a94bb850e658466
Arvind Thangamani <dnivra26@gmail.com>
2016-12-10 20:07:01 +0530

    added endpoint to move to next song

--- server/main.js
+++ server/main.js
@@ -1,5 +1,12 @@
 import { Meteor } from 'meteor/meteor';
-import '../api/tasks.js';
+import { Tasks } from '../api/tasks.js';
 Meteor.startup(() => {
   // code to run on server at startup
+    JsonRoutes.add("get", "/next", function (req, res, next) {
+        const song = Tasks.find({}, { sort: { requested_at: 1 } }).fetch()[0];
+        Tasks.remove(song._id);
+        JsonRoutes.sendResult(res, {
+            data: 'next'
+        });
+    });
 });
