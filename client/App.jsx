7a54faab22b4869c9e06ee657d7ece1ac5629597
Arvind Thangamani <dnivra26@gmail.com>
2016-12-10 19:50:02 +0530

    auto play next song when current song ends

--- client/App.jsx
+++ client/App.jsx
@@ -3,6 +3,7 @@ import YouTube from 'react-youtube';
 import 'whatwg-fetch';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Tasks } from '../api/tasks';
+import _ from 'lodash';
 //10.130.202.108
 class App extends Component {
 
@@ -10,24 +11,31 @@ class App extends Component {
         super();
         this.state = { id: ''};
     }
-    componentDidMount() {
-        const query = 'thalli pogathey';
-        const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&maxResults=1&key=AIzaSyCH1CsGdCFdEV2NFvpiDoYyblqO56mmg8Y`;
-        const this2 = this;
-        fetch(url)
-            .then(function(response) {
-                return response.json()
-            }).then(function(json) {
-            this2.setState({id: json.items[0].id.videoId});
-        }).catch(function(ex) {
-            console.log('parsing failed', ex)
-        });
+    componentWillReceiveProps(nextProps) {
+        console.log(nextProps);
+        const {request_url, request_string} = nextProps.song;
+        console.log(request_url, request_string);
+        if(_.isUndefined(request_url)) {
+            const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${request_string}&type=video&maxResults=1&key=AIzaSyCH1CsGdCFdEV2NFvpiDoYyblqO56mmg8Y`;
+            const this2 = this;
+            fetch(url)
+                .then(function(response) {
+                    return response.json()
+                }).then(function(json) {
+                    console.log(json);
+                this2.setState({id: json.items[0].id.videoId});
+            }).catch(function(ex) {
+                console.log('parsing failed', ex)
+            });
+        } else {
+            this.setState({id: request_url.split('=')[1]});
+        }
     }
     handleEnd() {
-        console.log('song has ended');
+        console.log('song has ended', this.props);
+        Tasks.remove(this.props.song._id);
     }
     render() {
-        console.log('songs', this.props.song);
         const opts = {
             height: '390',
             width: '640',
@@ -53,6 +61,6 @@ App.propTypes = {
 
 export default createContainer(() => {
     return {
-        song: Tasks.find({}).fetch(),
+        song: Tasks.find({}, { sort: { requested_at: 1 } }).fetch()[0],
     };
 }, App);
